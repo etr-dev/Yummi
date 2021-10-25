d3dfbff8e002741f0bf112e913ca093d3e62aec4
erobin27 <elijah1998robinson@gmail.com>
2021-10-25 05:07:28 -0400

    upload popup works

--- src/components/uploadCard.js
+++ src/components/uploadCard.js
@@ -3,12 +3,14 @@ import { Box, makeStyles, Typography, Card , CardContent, Grid, Button } from '@
 import GoogleIcon from '@mui/icons-material/Google';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import AppleIcon from '@mui/icons-material/Apple';
-
+import {useDropzone} from 'react-dropzone';
+import UploadRoundedIcon from '@mui/icons-material/UploadRounded';
+import svgUpload from '../images/backgrounds/upload_675x900.svg'
 const useStyles = makeStyles((theme) => {
     return {
-    root: {
-        display: 'flex'
-    },
+        root: {
+            display: 'flex'
+        },
         card: {
             backgroundColor: theme.palette.primary.main,
             display: 'flex',
@@ -16,67 +18,74 @@ const useStyles = makeStyles((theme) => {
             alignItems: 'center',
             paddingLeft: '20px',
             paddingRight: '20px',
-            paddingTop: '50px',
-            paddingBottom: '50px',
-            boxShadow: '0'
+            paddingTop: '10px',
+            paddingBottom: '10px',
+            boxShadow: '0px 0px 20px -5px',
         },
-        content: {
-            width: '200%'
-        },
-        yummi: {
+        titleDiv: {
             display: 'flex',
+            flexDirection: 'column',
             alignItems: 'center',
-            padding: 50
+            marginBottom: '60px'
         },
-        signin: {
+        uploadDiv: {
+            marginTop: 10,
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center'
-
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+            cursor: 'pointer',
+            height: 200,
+            width: '80%',
+            backgroundColor: theme.palette.secondary.main,
+            borderRadius: '10%',
+            border: `dashed 3px  ${theme.palette.info.main}`,
+            boxShadow: '0px 0px 20px -5px',
         },
-        title: {
-           marginTop:  -30
+        icon: {
+            transform: 'scale(4)',
+            color: theme.palette.text.primary
         },
-        logo: {
-            height: '150px'
-        },
-        buttonDiv: {
-            width: '100%',
+        x: {
+            marginTop: 20,
             display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-        },
-        button: {
-            width: '75%',
-            marginTop: '20px',
-            //fontSize: 25
+            flexDirection: 'row-reverse'
         }
     }
   })
   
-
-export default function LoginCard() {
+export default function LoginCard(props) {
     const classes = useStyles()
+    
+    const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+    
+    const files = acceptedFiles.map(file => (
+      console.log(file)
+    ));
+
     return (
         <Box borderRadius='5%' className={ classes.card }>
-            <div className={classes.yummi}>
-                <img src='Yummi.svg' alt="logo" className={ classes.logo } />
-                <div className={classes.signin}>
-                    <Typography variant='h4' color='textSecondary' >Sign-in to</Typography> 
-                    <Typography variant='h1' color='textPrimary' className={classes.title}>Yummi</Typography>
-                </div>
+            <div className={classes.titleDiv}>
+                <Typography variant='h3' color='textPrimary' className={ classes.none }>
+                    Drag and Drop
+                </Typography>
+                <Typography variant='h5' color='textPrimary'>
+                    Your data below
+                </Typography>
             </div>
 
-            <Typography variant='h6' color='textPrimary' className={ classes.none }>
-                Login through one of the methods below...
+            <Typography variant='h6' color='textPrimary' align='center'>
+                accepted file types: <br/> .csv .xlx
             </Typography>
 
-            <div className = { classes.buttonDiv }>
-                <Button className={ classes.button } size={ 'large' } color='secondary' variant='contained' startIcon={ <GoogleIcon /> }  >Google</Button>
-                <Button className={ classes.button } size={ 'large' } color='secondary' variant='contained' startIcon={ <FacebookIcon /> }  >Facebook</Button>
-                <Button className = { classes.button } size={ 'large' } color='secondary' variant='contained' startIcon={ <AppleIcon  /> }  >Apple</Button>
+            <div {...getRootProps({className: 'dropzone'})} className={ classes.uploadDiv }>
+            <input {...getInputProps()} />
+                <Typography variant='h5' color='textPrimary' align='center'>
+                    Drag file here or click to browse
+                </Typography>
+                < UploadRoundedIcon className={ classes.icon }/>
             </div>
-
+            <Button className={classes.x} onClick={props.close}>close</Button>
         </Box>
       );
     }
