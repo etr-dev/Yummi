fddcf7fb2c995b146a315a5090e4158a702b585e
erobin27 <elijah1998robinson@gmail.com>
2021-11-04 00:03:22 -0400

    date selection css and highlighting

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { Card, CardContent, TextField } from "@material-ui/core/";
-import { LocalizationProvider, DesktopDatePicker } from "@mui/lab/";
+import { LocalizationProvider, DesktopDatePicker, PickersDay } from "@mui/lab/";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
-
+import { datesAreOnSameDay, inDateRange } from "../data/helpers";
 import {
   Chart,
   BarSeries,
@@ -32,9 +32,22 @@ const useStyles = makeStyles((theme) => {
     datepickerTextboxPalette: {
       color: theme.palette.text.primary,
       backgroundColor: theme.palette.secondary.main,
-      padding: '20px',
-      borderTopLeftRadius: '20px',
-      borderTopRightRadius: '20px',
+      padding: "20px",
+      borderTopLeftRadius: "20px",
+      borderTopRightRadius: "20px",
+    },
+    highlightedDay: {
+      color: `${theme.palette.text.primary} !important`,
+      backgroundColor: `${theme.palette.info.main} !important`,
+    },
+    selectedDay: {
+      border: `1px solid ${theme.palette.info.main} !important`,
+      color: `${theme.palette.text.primary} !important`,
+      backgroundColor: `${theme.palette.primary.main} !important`,
+    },
+    errorDays: {
+      color: `${theme.palette.primary.main} !important`,
+      backgroundColor: `red !important`,
     },
   };
 });
@@ -42,6 +55,9 @@ const useStyles = makeStyles((theme) => {
 export default function GraphCard(props) {
   const chartData = props.data;
   const classes = useStyles();
+  const [start, setStart] = React.useState(new Date());
+  const [end, setEnd] = React.useState(new Date());
+
   return (
     //todo: create clickevents for d/m/y buttons
     <Card elevation={0} className={classes.card}>
@@ -50,19 +66,86 @@ export default function GraphCard(props) {
           <LocalizationProvider dateAdapter={AdapterDateFns}>
             <DesktopDatePicker
               label="Start Date"
-              //value={value}
+              value={start}
               onChange={(newValue) => {
-                console.log(newValue);
+                setStart(newValue);
+              }}
+              renderDay={(day, selected, DayProps) => {
+                //https://stackoverflow.com/questions/61502954/highlighting-weekend-days-sat-and-sun-in-material-ui-date-picker
+                
+                
+                //highlight the start and end specially
+                if (
+                  datesAreOnSameDay(day, start) ||
+                  datesAreOnSameDay(day, end)
+                ) {
+                  return (
+                    <PickersDay
+                      {...DayProps}
+                      classes={{ root: classes.selectedDay }}
+                    />
+                  );
+                }
+
+                //Highlight selected dates in range of start to end
+                if (inDateRange(day, start, end)) {
+                  return (
+                    <PickersDay
+                      {...DayProps}
+                      classes={{ root: classes.highlightedDay }}
+                    />
+                  );
+                }
+
+                //Disable days that come after the end date (so that start !> end)
+                if (day > end) {
+                  return <PickersDay {...DayProps} disabled={true} />;
+                }
+                return <PickersDay {...DayProps} />;
               }}
               renderInput={(params) => <TextField {...params} />}
               InputProps={{ className: classes.datepickerTextboxPalette }}
             />
+
             <DesktopDatePicker
               label="End Date"
               color="secondary"
-              //value={value}
+              disableCloseOnSelect={false}
+              value={end}
               onChange={(newValue) => {
-                console.log(newValue);
+                setEnd(newValue);
+              }}
+              renderDay={(day, selected, DayProps) => {
+                //https://stackoverflow.com/questions/61502954/highlighting-weekend-days-sat-and-sun-in-material-ui-date-picker
+                
+                //highlight the start and end specially
+                if (
+                  datesAreOnSameDay(day, start) ||
+                  datesAreOnSameDay(day, end)
+                ) {
+                  return (
+                    <PickersDay
+                      {...DayProps}
+                      classes={{ root: classes.selectedDay }}
+                    />
+                  );
+                }
+
+                //Highlight selected dates in range of start to end
+                if (inDateRange(day, start, end)) {
+                  return (
+                    <PickersDay
+                      {...DayProps}
+                      classes={{ root: classes.highlightedDay }}
+                    />
+                  );
+                }
+
+                //Disable days that come before the start date (so that start !> end)
+                if (day < start) {
+                  return <PickersDay {...DayProps} disabled={true} />;
+                }
+                return <PickersDay {...DayProps} />;
               }}
               renderInput={(params) => <TextField {...params} />}
               InputProps={{ className: classes.datepickerTextboxPalette }}
