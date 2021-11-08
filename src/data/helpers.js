417dfc8dabef0153339b38817c570861fdec5b00
erobin27 <elijah1998robinson@gmail.com>
2021-11-08 18:05:01 -0500

    fixed date range not being inclusive

--- src/data/helpers.js
+++ src/data/helpers.js
@@ -1,11 +1,10 @@
 export function inDateRange(day, start, end) {
-    if (day > start && day < end)
-        return true;
-    else
-        return false;
+  if (datesAreOnSameDay(day, start) || datesAreOnSameDay(day, end)) return true;
+  if (day > start && day < end) return true;
+  else return false;
 }
 
 export const datesAreOnSameDay = (first, second) =>
-    first.getFullYear() === second.getFullYear() &&
-    first.getMonth() === second.getMonth() &&
-    first.getDate() === second.getDate();
+  first.getFullYear() === second.getFullYear() &&
+  first.getMonth() === second.getMonth() &&
+  first.getDate() === second.getDate();
