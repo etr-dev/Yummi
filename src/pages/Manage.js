de5db792e9fb02657d94a19e4c8675633c0dbe2e
sdehart4 <sdehart4@uncc.edu>
2021-11-09 18:51:47 -0500

    manage page drawer update(pulls user files)

--- src/pages/Manage.js
+++ src/pages/Manage.js
@@ -3,6 +3,11 @@ import { makeStyles, Typography, Button, Grid } from "@material-ui/core"; //put 
 import Popup from "../components/popup";
 import LoginCard from "../components/loginCard";
 import MyDrawer from "../components/drawer";
+import axios from "axios";
+import {findUser} from "../data/database"
+import { useAuth0 } from "@auth0/auth0-react";
+import { drawerSelection } from "../data/Redux/Actions/index";
+import { useSelector } from "react-redux";
 
 /*
 
@@ -36,51 +41,32 @@ const useStyles = makeStyles((theme) => {
   };
 });
 
-const itemNames = [
-  "Big Mac",
-  "Quarter Pounder with Cheese",
-  "Double Quarter Pounder with Cheese",
-  "Filet-O-Fish",
-  "2 Cheeseburgers",
-  "Southern Style Chicken",
-  "Bacon Clubhouse Burger",
-  "Chicken McNuggets - 20pc",
-  "Small French Fry",
-  "Medium French Fry",
-  "McDouble",
-  "Buffalo Ranch McChicken",
-  "Quarter Pounder with Cheese",
-  "Double Quarter Pounder with Cheese",
-  "Filet-O-Fish",
-  "2 Cheeseburgers",
-  "Southern Style Chicken",
-  "Bacon Clubhouse Burger",
-  "Chicken McNuggets - 20pc",
-  "Small French Fry",
-  "Medium French Fry",
-  "McDouble",
-  "Buffalo Ranch McChicken",
-  "Small French Fry",
-  "Medium French Fry",
-  "McDouble",
-  "Buffalo Ranch McChicken",
-  "Quarter Pounder with Cheese",
-  "Double Quarter Pounder with Cheese",
-  "Filet-O-Fish",
-  "2 Cheeseburgers",
-  "Southern Style Chicken",
-  "Bacon Clubhouse Burger",
-  "Chicken McNuggets - 20pc",
-  "Small French Fry",
-  "Medium French Fry",
-  "McDouble",
-  "END",
-];
-
 //this function is what creates the page that will be loaded by App.js
 export default function Manage() {
   //variables needed in the return statement are created here
+  const [filelist, setfilelist] = React.useState([]);
+  const drawerSelection = useSelector((state) => state.drawer);
+  let files 
+  const { user } = useAuth0();
   const classes = useStyles();
+
+  React.useEffect(()=>{
+    axios(findUser(user.email))
+      .then((res) => {
+        const dbUser = res.data[0]; //get the user object from the Database
+        files = dbUser.files
+        for(let i = 0; i < files.length; i++){
+            setfilelist(filelist => [...filelist, files[i].fileInfo.filename]);
+        }
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
+    },[]);
+
+  React.useEffect(()=>{
+    console.log(drawerSelection)
+  },[drawerSelection]);
 
   //The return statement returns JSX code (it is just HTML in javascript basically)
   //This is what will be returned when we call the function in App.js
@@ -94,7 +80,8 @@ export default function Manage() {
         {/* LIST DRAWER */}
         <Grid container>
           <Grid className={classes.drawer} item xs={12} md={3} lg={2}>
-            <MyDrawer itemNames={itemNames} />
+            <MyDrawer itemNames={filelist} />
+            {console.log(filelist)}
           </Grid>
           {/* CHART */}
           <Grid className={classes.grid} item xs={12} md={9} lg={10}>
